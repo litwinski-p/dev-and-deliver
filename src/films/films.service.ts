30b37936447b41e9e13a7e13d16d9bfd560d215c
Piotr Litwiński <pli@unatrix.com>
2024-12-04 22:37:59 +0100

    Inject film repository to films service

--- src/films/films.service.ts
+++ src/films/films.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateFilmInput } from './dto/create-film.input';
 import { UpdateFilmInput } from './dto/update-film.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Film } from './entities/film.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class FilmsService {
+
+  constructor(@InjectRepository(Film) private readonly filmRepository: Repository<Film>) {
+  }
+
   create(createFilmInput: CreateFilmInput) {
     return 'This action adds a new film';
   }
 
-  findAll() {
-    return `This action returns all films`;
+  async findAll() {
+    return this.filmRepository.find();
   }
 
   findOne(id: number) {
