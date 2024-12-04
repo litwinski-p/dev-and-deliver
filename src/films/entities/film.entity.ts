984d4887902129475b5077c7291adf564722ec8a
Piotr Litwiński <pli@unatrix.com>
2024-12-04 22:29:33 +0100

    Update film entity

--- src/films/entities/film.entity.ts
+++ src/films/entities/film.entity.ts
@@ -1,7 +1,18 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Film {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  name: string;
+
+  @Field()
+  @Column()
+  description: string;
 }
