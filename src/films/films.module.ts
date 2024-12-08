fcb0ba4863516886aeeb5d883ef3eea909a8de82
Piotr Litwiński <pli@unatrix.com>
2024-12-09 00:27:45 +0100

    Load people data into DB on the application bootstrap

--- src/films/films.module.ts
+++ src/films/films.module.ts
@@ -3,9 +3,13 @@ import { FilmsService } from './films.service';
 import { FilmsResolver } from './films.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Film } from './entities/film.entity';
+import { People } from './entities/people.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Film])],
+  imports: [
+    TypeOrmModule.forFeature([Film]),
+    TypeOrmModule.forFeature([People]),
+  ],
   providers: [FilmsResolver, FilmsService],
 })
 export class FilmsModule {
--- src/films/films.service.ts
+++ src/films/films.service.ts
@@ -1,22 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Film } from './entities/film.entity';
 import { Repository } from 'typeorm';
 import axios from 'axios';
 import { isExpired } from '../utils/helpers';
+import { People } from './entities/people.entity';
 
 @Injectable()
-export class FilmsService {
+export class FilmsService implements OnApplicationBootstrap {
   private readonly filmsApiUrl = 'https://swapi.dev/api/films';
+  private readonly peopleApiUrl = 'https://swapi.dev/api/people';
 
-  constructor(@InjectRepository(Film) private readonly filmRepository: Repository<Film>) {
+  constructor(
+    @InjectRepository(Film) private readonly filmRepository: Repository<Film>,
+    @InjectRepository(People) private readonly peopleRepository: Repository<People>,
+  ) {
   }
+
+  async onApplicationBootstrap(): Promise<any> {
+    let people = await this.peopleRepository.find();
+
+    if (people.length === 0 || (people.length > 0 && isExpired(people[0]))) {
+      console.log('Loading people data...');
+
+      let nextUrl = this.peopleApiUrl;
+      const entities: People[] = [];
+
+      while (nextUrl) {
+        try {
+          const { data: { next, results: peopleData } } = await axios.get(nextUrl);
+
+          peopleData.forEach((item) => {
+            const entity = this.peopleRepository.create({
+              name: item.name,
+            });
+
+            entities.push(entity);
+          });
+
+          nextUrl = next;
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      await this.peopleRepository.save(entities);
+      console.log('People data loaded.');
+    }
+  }
+
 
   async findAll() {
     let films = await this.filmRepository.find();
 
     if (films.length === 0 || (films.length > 0 && isExpired(films[0]))) {
-
       try {
         const { data: { results: filmsData } } = await axios.get(this.filmsApiUrl);
 
